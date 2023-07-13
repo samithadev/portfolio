57ace6dae22cd5ff7fa1ecd149fc48c5239b7476
Samitha Wickramasinghe <89331663+samithadev@users.noreply.github.com>
2023-07-13 15:02:53 +0530

    Update Model.jsx

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -7,7 +7,7 @@ export default function Model({activeID, setShowModel}) {
 
   return (
     <div className='w-full h-full fixed top-0 left-0 z-10 bg-headingColor bg-opacity-40'>
-        <div className='w-11/12 md:max-w-[400px] md:w-full mt-[30px]  absolute top-1/2 left-1/2 z-20 bg-white rounded-[8px] transform -translate-x-1/2 -translate-y-1/2 p-5 '>
+        <div className='w-11/12 md:max-w-[600px] md:w-full mt-[30px]  absolute top-1/2 left-1/2 z-20 bg-white rounded-[8px] transform -translate-x-1/2 -translate-y-1/2 p-5 '>
             <div>
                 <figure>
                     <img className='rounded-[8px] w-[200px]' src={portfolio.imgUrl} alt=''/>
